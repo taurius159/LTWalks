bcbd1fc5099d321694d3dbfc066c06071cfe84d9
Taurius <ytaurius159@email.com>
2024-05-24 12:17:17 +0300

    DbContext added

--- Api/Data/LTWalksDbContext.cs
+++ Api/Data/LTWalksDbContext.cs
@@ -1,8 +1,16 @@
+using Api.Models.Domains;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data;
 
 public class LTWalksDbContext : DbContext
 {
+    public LTWalksDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions)
+    {
+        
+    }
 
+    public DbSet<Difficulty> Difficulties { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Walk> Walks { get; set; }
 }
--- Api/Models/Domain/Region.cs
+++ Api/Models/Domain/Region.cs
@@ -1,7 +1,9 @@
 namespace Api.Models.Domains;
 
-public class Difficulty
+public class Region
 {
     public Guid Id { get; set; }
-    public int Name { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? RegionImageUrl { get; set; }
 }
