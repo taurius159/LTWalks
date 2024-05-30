0b71f60953e090646442d2d8250114a0c69875fb
Taurius <ytaurius159@email.com>
2024-05-30 09:18:07 +0300

    correct inheritance for auth context

--- Api/Data/LTWalksAuthDbContext.cs
+++ Api/Data/LTWalksAuthDbContext.cs
@@ -1,10 +1,10 @@
-using Api.Models.Domains;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data;
 
-public class LTWalksAuthDbContext : DbContext
+public class LTWalksAuthDbContext : IdentityDbContext
 {
     public LTWalksAuthDbContext(DbContextOptions<LTWalksAuthDbContext> dbContextOptions) :base(dbContextOptions)
     {
@@ -13,6 +13,8 @@ public class LTWalksAuthDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         var readerRoleId = "7dce9596-474b-45d5-9c7d-82dcee035f40";
         var writeRoleId = "508d2df0-4b8a-4a44-a745-890148ebc3bb";
 
--- Api/Migrations/LTWalksAuthDb/20240530061645_authContextInheritIdentityDbContext.Designer.cs
+++ Api/Migrations/LTWalksAuthDb/20240530061645_authContextInheritIdentityDbContext.Designer.cs
@@ -4,6 +4,7 @@ using Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api.Migrations.LTWalksAuthDb
 {
     [DbContext(typeof(LTWalksAuthDbContext))]
-    partial class LTWalksAuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240530061645_authContextInheritIdentityDbContext")]
+    partial class authContextInheritIdentityDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
